663ce8dbc3d7202210d0377d910410a3df052e3d
seokhawn01 <skhu202214139@gmail.com>
2025-09-13 15:30:29 +0900

    출력 메세지 수정

--- src/git_test2/Hello.java
+++ src/git_test2/Hello.java
@@ -1,1 +1,1 @@
-package git_test2; public class Hello { public void say() { System.out.println("hello world"); } }
+package git_test2; public class Hello { public void say() { System.out.println("안녕하세요"); } }
