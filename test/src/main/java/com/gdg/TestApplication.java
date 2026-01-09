8973d03707bc7aff604474fb622887a1a4049987
seokhawn01 <skhu202214139@gmail.com>
2026-01-09 21:14:24 +0900

    찐막수정

--- test/src/main/java/com/gdg/TestApplication.java
+++ test/src/main/java/com/gdg/TestApplication.java
@@ -3,7 +3,7 @@ package com.gdg;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.gdg")
 public class TestApplication {
     public static void main(String[] args) {
         SpringApplication.run(TestApplication.class, args);
